7678ad44d533c5747fafd008d602fde8da94a8a3
mzhubail <mzhubail@gmail.com>
2023-11-09 15:39:39 +0300

    Setup app to run on android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.exhibithub.application',
   appName: 'App1',
   webDir: 'www',
   server: {
