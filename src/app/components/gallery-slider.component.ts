29730cc0197889ab78a2b32e394cec8d4a7def70
ahmedinho11y <ahmedinho.y@gmail.com>
2023-12-12 02:33:52 +0300

    gallery slider component

---
+++ src/app/components/gallery-slider.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'gallery-slider',
+  template: `
+    <div
+      id="carouselExampleCaptions"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="3"
+          aria-label="Slide 4"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="4"
+          aria-label="Slide 5"
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img
+            src="../assets/gallery1.jpeg"
+            class="d-block w-100"
+            style="border-radius:8px;"
+            alt="..."
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/gallery2.jpeg"
+            class="d-block w-100"
+            style="border-radius:8px;"
+            alt="..."
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/gallery3.jpeg"
+            class="d-block w-100"
+            style="border-radius:8px;"
+            alt="..."
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/gallery4.jpeg"
+            class="d-block w-100"
+            style="border-radius:8px;"
+            alt="..."
+          />
+        </div>
+        <div class="carousel-item">
+          <img
+            src="../assets/gallery5.jpeg"
+            class="d-block w-100"
+            style="border-radius:8px;"
+            alt="..."
+          />
+        </div>
+      </div>
+    </div>
+  `,
+})
+export class GallerySliderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
