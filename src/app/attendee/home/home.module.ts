6055fb4bc1d3f7ed64426f8f01098cc319b22d96
ahmedinho11y <ahmedinho.y@gmail.com>
2023-12-12 17:14:01 +0300

    home page styling

--- src/app/attendee/home/home.module.ts
+++ src/app/attendee/home/home.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,10 +7,17 @@ import { IonicModule } from '@ionic/angular';
 import { HomePageRoutingModule } from './home-routing.module';
 
 import { HomePage } from './home.page';
-import { GallerySliderComponent } from 'src/app/components/gallery-slider.component';
+import { ComponentsModule } from 'src/app/components/components.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
-  declarations: [HomePage, GallerySliderComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    HomePageRoutingModule,
+    ComponentsModule,
+  ],
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,24 +1,21 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormComponent, InputComponent } from './input-components.component';
-
-
+import { CardsSliderComponent } from './cards-slider.component';
+import { GallerySliderComponent } from './gallery-slider.component';
 
 var components = [
   InputComponent,
   FormComponent,
-
-]
+  GallerySliderComponent,
+  CardsSliderComponent,
+];
 
 @NgModule({
   declarations: components,
-  imports: [
-    CommonModule,
-    FormsModule,
-    ReactiveFormsModule,
-
-  ],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   exports: components,
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class ComponentsModule { }
+export class ComponentsModule {}
