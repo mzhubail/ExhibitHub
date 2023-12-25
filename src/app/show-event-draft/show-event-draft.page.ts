e58a3ba7739d84b890db7efccfcf5c7dff54c3c7
mzhubail <mzhubail@gmail.com>
2023-12-25 08:33:03 +0300

    Remove `@ts-nocheck` from shoe-event-draft.page

--- src/app/show-event-draft/show-event-draft.page.ts
+++ src/app/show-event-draft/show-event-draft.page.ts
@@ -1,9 +1,5 @@
-// @ts-nocheck
 import { Component, AfterViewInit, ElementRef, ViewChild } from '@angular/core';
-import { ItemReorderEventDetail } from '@ionic/core';
-import { AlertController } from '@ionic/angular';
-import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { CustomePageService, color } from '../services/custome-page.service';
+import { CustomePageService } from '../services/custome-page.service';
 
 @Component({
   selector: 'app-show-event-draft',
@@ -11,14 +7,9 @@ import { CustomePageService, color } from '../services/custome-page.service';
   styleUrls: ['./show-event-draft.page.scss'],
 })
 export class ShowEventDraftPage implements AfterViewInit {
-  public x = this.custPage.xx;
-  public y = this.custPage.yy;
-
-  public color = this.custPage.color;
-
-  @ViewChild('rectangle') rect: ElementRef;
-
-  constructor(public custPage: CustomePageService) {}
+  constructor(
+    public custPage: CustomePageService,
+  ) {}
 
   ngAfterViewInit() {}
 }
