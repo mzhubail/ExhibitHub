183d2947c8022c05c46edc7f788fcfcac1236cd2
Almutawaj <foetheuniversity2022@gmail.com>
2023-12-26 21:19:45 +0300

    Events Search Is Done

--- src/app/services/events.service.ts
+++ src/app/services/events.service.ts
@@ -16,22 +16,21 @@ import {
   query,
   DocumentData,
 } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Observable, debounceTime, map } from 'rxjs';
 import { CustomePageService, EventDesign } from './custome-page.service';
 import { Reservation } from './reservation.service';
-
 
 @Injectable({
   providedIn: 'root',
 })
 export class EventsService {
   public resAndEvents$!: Observable<ResAndEvent[]>;
+  public resAndEvents_filtered$!: Observable<ResAndEvent[]>;
 
-  constructor(public firestore: Firestore, public custom:CustomePageService) {
-    this.getResAndEvents().then(()=>{
+  constructor(public firestore: Firestore, public custom: CustomePageService) {
+    this.getResAndEvents().then(() => {
       console.log(this.resAndEvents$);
     });
-
   }
 
   async getResAndEvents() {
@@ -40,10 +39,9 @@ export class EventsService {
 
     const combinedResAndEvents: ResAndEvent[] = [];
 
-
-    events.forEach(async event => {
+    events.forEach(async (event) => {
       const matchingReservation = reservations.find(
-        reservation => reservation.id === event.id
+        (reservation) => reservation.id === event.id
       );
 
       if (matchingReservation) {
@@ -58,21 +56,19 @@ export class EventsService {
       }
     });
 
-    this.resAndEvents$ = new Observable(observer => {
+    this.resAndEvents$ = new Observable((observer) => {
       observer.next(combinedResAndEvents);
       observer.complete();
     });
 
-
-
-
+    this.resAndEvents_filtered$ = this.resAndEvents$;
   }
 
   async getEvents(): Promise<EventDesign[]> {
     const queryCollection = collection(this.firestore, 'Events');
     const snapshot = await getDocs(queryCollection);
     const data: EventDesign[] = [];
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       const docData = doc.data() as EventDesign;
       docData.id = doc.id;
       data.push(docData);
@@ -84,7 +80,7 @@ export class EventsService {
     const queryCollection = collection(this.firestore, 'Reservations');
     const snapshot = await getDocs(queryCollection);
     const data: Reservation[] = [];
-    snapshot.forEach(doc => {
+    snapshot.forEach((doc) => {
       const docData = doc.data() as Reservation;
       docData.id = doc.id;
       data.push(docData);
@@ -92,16 +88,25 @@ export class EventsService {
     return data;
   }
 
-
-
-  
+  searchEvents(event: any) {
+    const searchTerm = event.target.value.toLowerCase().trim();
+    if (!searchTerm) {
+      this.resAndEvents_filtered$ = this.resAndEvents$;
+      return; // exit
+    }
+    this.resAndEvents_filtered$ = this.resAndEvents$.pipe(
+      debounceTime(300),
+      map((elements: any[]) => {
+        return elements.filter((e: any) =>
+          e.event.title.toLowerCase().includes(searchTerm)
+        );
+      })
+    );
+  }
 }
 
-
-
-export interface ResAndEvent
-{
+export interface ResAndEvent {
   event: EventDesign;
-  reservation:Reservation;
-  imageUrl:string;
+  reservation: Reservation;
+  imageUrl: string;
 }
